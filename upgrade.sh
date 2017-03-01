f8784e4ead3cd303666e9490fb919a6272e98505
Bradley <bsakdol@turnitin.com>
2017-03-01 12:54:38 -0800

    Added git commands

--- upgrade.sh
+++ upgrade.sh
@@ -11,8 +11,16 @@ if [ "$(whoami)" = "root" ]; then
 
 fi
 
+# Pull Jumpbox updates
+COMMAND="${PREFIX}git checkout master"
+echo "Checkout master Git repo..."
+COMMAND="${PREFIX}git pull origin master"
+echo "Pull updated Jumpbox files..."
+
 # Install new Python packages
 COMMAND="${PREFIX}pip install -r requirements.txt --upgrade"
 echo "Updating required Python packages ($COMMAND)..."
 eval $COMMAND
 
+# Update complete notification
+echo "Jumpbox update complete."

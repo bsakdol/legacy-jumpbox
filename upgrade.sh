76e68d10f17c75251b8ff8a0ddc835f13ce137ab
Bradley <bsakdol@gmail.com>
2017-03-01 18:25:37 -0800

    Update directions

--- upgrade.sh
+++ upgrade.sh
@@ -3,7 +3,7 @@
 # This script will prepare Jumpbox to run after a release upgrade.
 #
 
-# If not already root, use "sudo" and prompt for password
+# If not already "root", script must be run with "sudo"
 if [ "$(whoami)" = "root" ]; then
         # As "root", ask the user to confirm before continuing
         read -n1 -rsp $'Running Jumpbox as root, press any key to continue or ^C to cancel\n'

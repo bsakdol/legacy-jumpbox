5693341f405d2b21289d97979a77eae04c79d7ed
Bradley <bsakdol@gmail.com>
2017-05-24 10:55:32 -0600

    Split db connector modules
    Split the db connector into two modules in order to facilitate ease of updates.

--- jumpbox/menu.py
+++ jumpbox/menu.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """This module handles the menu system"""
 
-from database import DBHandler
+from dbquery import DBHandler
 from formatter import FormatMenu
 
 
